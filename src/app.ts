2738913ad5c088156273c4ac3ade9d2d15136416
asifamingov <asif.amin@dsiti.qld.gov.au>
2022-07-01 16:37:47 +1000

    added search form in the app

--- src/app.ts
+++ src/app.ts
@@ -2,17 +2,16 @@
 import { FilterModule } from './modules/FilterModule'
 import { SearchModule } from './modules/SearchModule'
 
-/* eslint-disable-next-line: no-unused-vars */
 class initApp {
     static init() {
         const filterComponent = new FilterModule()
         const searchResults = new SearchModule()
     }
 }
-
-initApp.init();
-
-// initalize on history changes
+// initalize on history changes only
 window.onpopstate = function(event) {
     initApp.init();
 };
+// initalize on page reload
+initApp.init();
+
