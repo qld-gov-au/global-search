464f1fd97b634f9a02eb6d96fbc2d26576a4edcf
asifamingov <asif.amin@dsiti.qld.gov.au>
2022-07-11 11:34:38 +1000

    updated related search results fetch

--- src/template/related-search.ts
+++ src/template/related-search.ts
@@ -1,13 +1,40 @@
-import { html } from 'lit-html'
+import { html, render } from 'lit-html'
+import { urlParameterMap } from '../utils/urlParameter'
+import { fetchData } from '../utils/fetchData'
+import { mainTemplate } from './main'
+import { noResultsTemplate } from './no-results'
 
 export function relatedResultsTemplate (contextualNavigation: { categories: any; }) {
+  const onRelatedSearchClick = (e: any) => {
+    e.preventDefault()
+    const clickedHref = e.target.href
+    const clickedVal = e.target.textContent
+    const currUrlParameterMap = urlParameterMap();
+    (document.querySelector('.qg-site-search__component .qg-search-site__input') as HTMLInputElement).value = clickedVal
+
+    // push in the history stack
+    history.pushState({}, '', `${clickedHref}`)
+
+    // fetch the results
+    fetchData(clickedHref).then(data => {
+      const { contextualNavigation, results } = data?.response?.resultPacket
+      if (results?.length) {
+        render(mainTemplate(data?.response, currUrlParameterMap), document.getElementById('qg-search-results__container') as HTMLBodyElement)
+        render(relatedResultsTemplate(contextualNavigation), document.getElementById('related-search__tags')!)
+      } else {
+        render(noResultsTemplate('No results found'), document.getElementById('qg-search-results__container')!)
+        render('', document.getElementById('related-search__tags')!)
+      }
+    })
+  }
+
   if (contextualNavigation) {
     const { categories } = contextualNavigation
     if (categories[0]?.name === 'topic') {
       return html` <p class="related-search__title">Related search</p>
         <section class="related-search__tags test">
             ${categories[0]?.clusters.map((item: any) =>
-              html`<a href="${item.href}&start_rank=1" class="qg-btn btn-outline-dark m-1">${item.query}</a>`
+              html`<a @click="${(e : any) => onRelatedSearchClick(e)}" href="${item.href}&start_rank=1" class="qg-btn btn-outline-dark m-1">${item.query}</a>`
           )}
         </section>`
     }
