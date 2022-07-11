89cd59568ffdc4a7ffef8ed9c20c9b16beb0cd85
asifamingov <asif.amin@dsiti.qld.gov.au>
2022-07-11 12:26:53 +1000

    added click event types

--- src/types/clickEvents.ts
+++ src/types/clickEvents.ts
@@ -1,3 +1,3 @@
 export interface RelatedSearchClick {
-    preventDefault: () => void; target: { href: any; textContent: any }
+    preventDefault: () => void; target: { href: string; textContent: string }
 }
