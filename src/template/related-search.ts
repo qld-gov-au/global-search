9a7c88a51d12e87f3239d14e5f7b4c21e11f59c8
asifamingov <asif.amin@dsiti.qld.gov.au>
2022-07-11 12:25:05 +1000

    added click event types

--- src/template/related-search.ts
+++ src/template/related-search.ts
@@ -3,9 +3,10 @@ import { urlParameterMap } from '../utils/urlParameter'
 import { fetchData } from '../utils/fetchData'
 import { mainTemplate } from './main'
 import { noResultsTemplate } from './no-results'
+import { RelatedSearchClick } from '../types/clickEvents'
 
 export function relatedResultsTemplate (contextualNavigation: { categories: any; }) {
-  const onRelatedSearchClick = (e: { preventDefault: () => void; target: { href: any; textContent: any } }) => {
+  const onRelatedSearchClick = (e: RelatedSearchClick) => {
     e.preventDefault()
     const clickedHref = e.target.href
     const clickedVal = e.target.textContent
@@ -34,7 +35,7 @@ export function relatedResultsTemplate (contextualNavigation: { categories: any;
       return html` <p class="related-search__title">Related search</p>
         <section class="related-search__tags test">
             ${categories[0]?.clusters.map((item: any) =>
-              html`<a @click="${(e: { preventDefault: () => void; target: { href: any; textContent: any } }) => onRelatedSearchClick(e)}" href="${item.href}&start_rank=1" class="qg-btn btn-outline-dark m-1">${item.query}</a>`
+              html`<a @click="${(e: RelatedSearchClick) => onRelatedSearchClick(e)}" href="${item.href}&start_rank=1" class="qg-btn btn-outline-dark m-1">${item.query}</a>`
           )}
         </section>`
     }
