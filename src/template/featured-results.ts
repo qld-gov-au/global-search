0abb5ee1ed37540290c82650665165e651007bb4
ThrawnCA <shell_layer-github@yahoo.com.au>
2022-11-29 15:11:11 +1000

    [QOL-9166] drop debugging and add true URL to featured results

--- src/template/featured-results.ts
+++ src/template/featured-results.ts
@@ -4,12 +4,18 @@ export function featuredResultsTemplate (exhibits: any[]) {
   return html`<h2 class="search-results-summary">Featured results</h2>
     ${exhibits.map((item, index) => {
                 return html`
-                <!-- Exhibit item: ${item} -->
                     <article class="qg-card qg-card__light-theme qg-card__clickable">
                         <div class="content">
                             <div class="details">
-                                <h2 class="qg-card__title"><a href="https://find.search.qld.gov.au${item.linkUrl}" class="stretched-link">${item.titleHtml}</a></h2>
-                                <p>${item.descriptionHtml}</p>
+                                <h2 class="qg-card__title">
+                                    <a href="https://find.search.qld.gov.au${item.linkUrl}" class="stretched-link">${item.titleHtml}</a>
+                                </h2>
+                                <ul class="qg-search-results__results-list">
+                                    <li class="description">${item.descriptionHtml}</li>
+                                    <li class="meta">
+                                        <span class="qg-search-results__url">${item.displayUrl}</span>
+                                    </li>
+                                </ul>
                             </div>
                         </div>
                     </article>`
