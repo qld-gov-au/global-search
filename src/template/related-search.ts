bffd8724fff5def2d77dea25c4ecafb87375ed80
ghazal-salehi <ghazal.salehi@smartservice.qld.gov.au>
2023-08-08 11:28:07 +1000

    QOLDEV-504 Looping through all the categories instead on first one only

--- src/template/related-search.ts
+++ src/template/related-search.ts
@@ -32,13 +32,15 @@ export function relatedResultsTemplate (contextualNavigation: { categories: any;
 
   if (contextualNavigation) {
     const { categories } = contextualNavigation
-    if (categories[0]?.name === 'topic') {
-      return html` <p class="related-search__title">Related search</p>
+    categories.array.forEach((element: any) => {
+      if (element?.name === 'topic') {
+        return html` <p class="related-search__title">Related search</p>
         <section class="related-search__tags">
             ${categories[0]?.clusters.map((item: any) =>
               html`<a @click="${(e: RelatedSearchClick) => onRelatedSearchClick(e)}" href="${item.href}&start_rank=1" class="qg-btn btn-outline-dark m-1">${item.query}</a>`
           )}
         </section>`
-    }
+      }
+    });
   }
 }
