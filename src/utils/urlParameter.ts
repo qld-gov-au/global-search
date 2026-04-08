aeac694dd8f3bbb7cc0ac99fae67ec0ac2d1ba8e
Elvi Shu <elvi.shu@smartservice.qld.gov.au>
2026-04-08 16:55:34 +1000

    [QOLDEV-1283]Replace legacy collection with default to handle hardcoded collection on agency's search forms.

--- src/utils/urlParameter.ts
+++ src/utils/urlParameter.ts
@@ -1,16 +1,25 @@
+import { DEFAULT_SEARCH_COLLECTION } from './constants'
+
 export const urlParameterMap = () => {
   const url = new URL(window.location.href)
   const urlParams = new URLSearchParams(url.search)
   const rSpe = (str: string | '') => {
     return str?.replace(/[\\#+()$~%*?<>{}]/g, '')
   }
+
+  const collectionParam = (urlParams.get('collection') || '').replace(/\%7E/g, '~')
+
+  // Exception to replace legacy Funnelback collection 'qgov~sp-search' with the DXP Funnelback defined on DEFAULT_SEARCH_COLLECTION.
+  // To prevent breaking on some agencies that still have hardcoded 'qgov~sp-search' on their search form.
+  const collection = collectionParam === 'qgov~sp-search' ? DEFAULT_SEARCH_COLLECTION : collectionParam
+
   return {
     query: rSpe((urlParams.get('query') || '').replace(/\%2F/g, '/')),
     profile: rSpe(urlParams.get('profile') || ''),
     label: rSpe(urlParams.get('label') || ''),
     filter: rSpe(urlParams.get('filter') || ''),
     startRank: parseInt(urlParams.get('start_rank') as string) || 1,
-    collection: (urlParams.get('collection') || '').replace(/\%7E/g, '~'),
+    collection,
     scope: rSpe(urlParams.get('scope') || ''),
     activePage: parseInt(urlParams.get('page') as string) || 1
   }
