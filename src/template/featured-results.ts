95469d7573eebe98ba94cb0d5376492b806dfa2f
Elvi Shu <elvi.shu@smartservice.qld.gov.au>
2026-04-09 15:39:48 +1000

    [QOLDEV-1283]Tidy up css class and add container div for Featured results

--- src/template/featured-results.ts
+++ src/template/featured-results.ts
@@ -3,23 +3,25 @@ import { SEARCH_BASE_URL } from '../utils/constants'
 
 export function featuredResultsTemplate (exhibits: any[]) {
   return html`
-    <h2 class="search-results-summary mb-4 mt-0">Featured results</h2>
-    <div class="card-group mb-5">
-    ${exhibits.map((item, index) => {
-        return html`
-            <article class="card card__light-theme">
-                <div class="card-body">
-                    <h3 class="card-title fs-5">
-                        <a href="${SEARCH_BASE_URL}${item.linkUrl}" class="stretched-link">${item.titleHtml}</a>
-                    </h3>
-                    <p class="card-text">
-                        ${item.descriptionHtml}
-                    </p>
-                </div>
-            </article>
-        `
-        }
-    )}
+    <div id="qld-featured-results">
+        <h2 class="mt-0">Featured results</h2>
+        <div class="card-group mb-5">
+        ${exhibits.map((item, index) => {
+            return html`
+                <article class="card card__light-theme">
+                    <div class="card-body">
+                        <h3 class="card-title fs-5">
+                            <a href="${SEARCH_BASE_URL}${item.linkUrl}" class="stretched-link">${item.titleHtml}</a>
+                        </h3>
+                        <p class="card-text">
+                            ${item.descriptionHtml}
+                        </p>
+                    </div>
+                </article>
+            `
+            }
+        )}
+        </div>
     </div>
   `
 }
