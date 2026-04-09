0d564aebad9d73472bdbcce578aad6752c6c99bf
Elvi Shu <elvi.shu@smartservice.qld.gov.au>
2026-04-09 19:18:30 +1000

    [QOLDEV-1283]Add hint styles on error details

--- src/template/error-results.ts
+++ src/template/error-results.ts
@@ -3,7 +3,7 @@ import { unsafeHTML } from 'lit-html/directives/unsafe-html'
 
 export function errorResultsTemplate (err: any, endpoint: string) {
   const errorMessage = err?.message || String(err)
-  const hint = errorMessage ? `Reference details: <ul><li>${errorMessage}</li><li>Endpoint: ${endpoint}</li></ul>` : ''
+  const hint = errorMessage ? `Reference details: <ul class="qld-hint-text"><li>${errorMessage}</li><li>Endpoint: ${endpoint}</li></ul>` : ''
   const message = `<div class="qld-search-results__error-hint"><p>Sorry, we encountered an error while loading the search results. Please try again later.</p><p>${hint}</p></div>`
   
   return html`${unsafeHTML(message)}`
