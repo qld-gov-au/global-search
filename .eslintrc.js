f2d12241a0a67a6936bd9ee70d3c30fe5eb22bb3
Elvi Shu <elvi.shu@smartservice.qld.gov.au>
2026-04-28 12:31:46 +1000

    [QOLDEV-1283]Eslint enforce semicolon on javaScript and disable on TypeScript

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,5 +16,21 @@ module.exports = {
   ],
   rules: {
     indent: ['error', 2, { "SwitchCase": 1 }],
-  }
+  },
+  overrides: [
+    {
+      // 1. Enforce semicolons for JS files
+      files: ["*.js", "*.jsx"],
+      rules: {
+        "semi": ["error", "always"]
+      }
+    },
+    {
+      // 2. Disable semicolons for TS files
+      files: ["*.ts", "*.tsx"],
+      rules: {
+        "@typescript-eslint/semi": ["error", "never"]
+      }
+    }
+  ]
 }
