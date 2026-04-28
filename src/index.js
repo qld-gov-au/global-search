59bd8d74ea2e682665c107c91573853ee443f2f0
Elvi Shu <elvi.shu@smartservice.qld.gov.au>
2026-04-28 12:30:25 +1000

    [QOLDEV-1283]Local test env - load and initialise each component independently

--- src/index.js
+++ src/index.js
@@ -1,79 +1,86 @@
-import Handlebars from "handlebars";
-import QGDSBundle from "@qld-gov-au/qgds-bootstrap5/dist/assets/node/handlebars.init.min.js";
+import Handlebars from 'handlebars';
+import QGDSBundle from '@qld-gov-au/qgds-bootstrap5/dist/assets/node/handlebars.init.min.js';
 
-import dataSkipLinks from "@qld-gov-au/qgds-bootstrap5/dist/sample-data/skiplinks/skipLinks.data.json";
-import dataHeader from "@qld-gov-au/qgds-bootstrap5/dist/sample-data/header/header.variant.masterBrand.data.json";
-import dataNavbar from "@qld-gov-au/qgds-bootstrap5/dist/sample-data/navbar/navbar.data.json";
-import dataBreadcrumbs from "@qld-gov-au/qgds-bootstrap5/dist/sample-data/breadcrumbs/breadcrumbs.data.json";
-import dataFooter from "@qld-gov-au/qgds-bootstrap5/dist/sample-data/footer/footer.data.json";
+import dataSkipLinks from '@qld-gov-au/qgds-bootstrap5/dist/sample-data/skiplinks/skipLinks.data.json';
+import dataHeader from '@qld-gov-au/qgds-bootstrap5/dist/sample-data/header/header.variant.masterBrand.data.json';
+import dataNavbar from '@qld-gov-au/qgds-bootstrap5/dist/sample-data/navbar/navbar.data.json';
+import dataBreadcrumbs from '@qld-gov-au/qgds-bootstrap5/dist/sample-data/breadcrumbs/breadcrumbs.data.json';
+import dataFooter from '@qld-gov-au/qgds-bootstrap5/dist/sample-data/footer/footer.data.json';
 
-// Function to load Handlebars, its helpers and precompiled templates
-async function loadHandlebars() {
-    window.Handlebars = Handlebars;
-    Handlebars.templates = Handlebars.templates || {};
+// Initialise Handlebars and register its helpers and partials
+function initHandlebars () {
+  window.Handlebars = Handlebars;
+  Handlebars.templates = Handlebars.templates || {};
 
-    // Bundled of helpers and partials with init(Handlebars)
-    QGDSBundle.init(Handlebars);
+  // Bundled of helpers and partials with init(Handlebars)
+  QGDSBundle.init(Handlebars);
+}
 
-    // Load precompiled templates
-    await import("../qgds-bootstrap5-release-precompiled/skipLinks.precompiled.js");
-    await import("../qgds-bootstrap5-release-precompiled/header.precompiled.js");
-    await import("../qgds-bootstrap5-release-precompiled/navbar.precompiled.js");
-    await import("../qgds-bootstrap5-release-precompiled/breadcrumbs.precompiled.js");
-    await import("../qgds-bootstrap5-release-precompiled/footer.precompiled.js");
+// Load and initialise a template, import precompiled template
+async function loadAndInitTemplate ({ importTemplate, componentName, data, containerId }) {
+  await importTemplate();
+
+  initTemplate({
+    componentName,
+    data,
+    containerId
+  });
 }
 
 // Main function to render all templates with their respective sample data
-async function initTemplates() {
-    await loadHandlebars();
+async function initTemplates () {
+  initHandlebars();
 
-    initTemplate({
-        componentName: 'skipLinks',
-        data: dataSkipLinks,
-        containerId: 'skiplinks-container'
-    });
-    initTemplate({
-        componentName: 'header',
-        data: dataHeader,
-        containerId: 'header-container'
-    });
-    initTemplate({
-        componentName: 'navbar',
-        data: dataNavbar,
-        containerId: 'navbar-container'
-    });
-    initTemplate({
-        componentName: 'breadcrumbs',
-        data: dataBreadcrumbs.default,
-        containerId: 'breadcrumb-container'
-    });
-
-    initTemplate({
-        componentName: 'footer',
-        data: { ...dataFooter, variantClass: 'dark' },
-        containerId: 'footer-container'
-    });
-
+  loadAndInitTemplate({
+    importTemplate: () => import('../qgds-bootstrap5-release-precompiled/skipLinks.precompiled.js'),
+    componentName: 'skipLinks',
+    data: dataSkipLinks,
+    containerId: 'skiplinks-container'
+  });
+  loadAndInitTemplate({
+    importTemplate: () => import('../qgds-bootstrap5-release-precompiled/header.precompiled.js'),
+    componentName: 'header',
+    data: dataHeader,
+    containerId: 'header-container'
+  });
+  loadAndInitTemplate({
+    importTemplate: () => import('../qgds-bootstrap5-release-precompiled/navbar.precompiled.js'),
+    componentName: 'navbar',
+    data: dataNavbar,
+    containerId: 'navbar-container'
+  });
+  loadAndInitTemplate({
+    importTemplate: () => import('../qgds-bootstrap5-release-precompiled/breadcrumbs.precompiled.js'),
+    componentName: 'breadcrumbs',
+    data: dataBreadcrumbs.default,
+    containerId: 'breadcrumb-container'
+  });
+  loadAndInitTemplate({
+    importTemplate: () => import('../qgds-bootstrap5-release-precompiled/footer.precompiled.js'),
+    componentName: 'footer',
+    data: { ...dataFooter, variantClass: 'dark' },
+    containerId: 'footer-container'
+  });
 }
 
 // Helper function to get template html and render into the page (by containerId)
-function initTemplate({ componentName, data, containerId }) {
-    const template = Handlebars.templates[`${componentName}.hbs`];
-    if (!template) {
-        console.error(`Template ${componentName}.hbs not found`);
-        return;
-    }
-    
-    const container = document.getElementById(containerId);
-    if (!container) {
-        console.error(`Container #${containerId} not found`);
-        return;
-    }
+function initTemplate ({ componentName, data, containerId }) {
+  const template = Handlebars.templates[`${componentName}.hbs`];
+  if (!template) {
+    console.error(`Template ${componentName}.hbs not found`);
+    return;
+  }
 
-    const html = template(data);
-    if (html) {
-        container.outerHTML = html;
-    }
+  const container = document.getElementById(containerId);
+  if (!container) {
+    console.error(`Container #${containerId} not found`);
+    return;
+  }
+
+  const html = template(data);
+  if (html) {
+    container.outerHTML = html;
+  }
 }
 
 // Init all the required templates
